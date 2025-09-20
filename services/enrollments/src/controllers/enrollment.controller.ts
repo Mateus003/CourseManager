461eeefb132a740cb0447fa8bb17462ce30e9b38
Mateus <mateusdev0101@gmail.com>
2025-09-20 16:10:15 -0300

    feat(enrollments): adiciona DTO, ValidationPipe e altera endpoint no controller

--- services/enrollments/src/controllers/enrollment.controller.ts
+++ services/enrollments/src/controllers/enrollment.controller.ts
@@ -1,23 +1,24 @@
-// src/controllers/enrollment.controller.ts
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { EnrollmentService } from '../services/enrollment.service';
+import { CreateEnrollmentDto } from '../dtos/create-enrollment.dto';
 
-@Controller('matriculas')
+@Controller() 
 export class EnrollmentController {
   constructor(private readonly enrollmentService: EnrollmentService) {}
 
-  @Post('matricula')
-  async create(@Body() body: { studentId: string; courseId: string }) {
-    const { studentId, courseId } = body;
+  @Post()
+  @UsePipes(new ValidationPipe()) 
+  async create(@Body() createEnrollmentDto: CreateEnrollmentDto) {
+    const { studentId, courseId } = createEnrollmentDto;
     return this.enrollmentService.create(studentId, courseId);
   }
 
-  @Get('aluno/:id')
+  @Get('students/:id')
   async getByStudent(@Param('id') studentId: string) {
     return this.enrollmentService.findByStudent(studentId);
   }
 
-  @Get('curso/:id')
+  @Get('courses/:id')
   async getByCourse(@Param('id') courseId: string) {
     return this.enrollmentService.findByCourse(courseId);
   }
