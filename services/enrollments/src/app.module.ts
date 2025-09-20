8cd895e52e446881d1cc1d2eb5801a7f95717c08
Mateus <mateusdev0101@gmail.com>
2025-09-20 16:06:12 -0300

    chore(enrollments): atualiza imports e configuração no app.module.ts

--- services/enrollments/src/app.module.ts
+++ services/enrollments/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EnrollmentService } from './services/enrollment.service';
+import { PrismaModule } from './prisma/prisma.module';
+import {EnrollmentController} from "./controllers/enrollment.controller";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule],
+  controllers: [EnrollmentController],
+  providers: [EnrollmentService],
 })
 export class AppModule {}
